5a5674ba51de300c1b0f65c2fa177b43ee8b0013
unknown <abdc5307@gmail.com>
2026-03-26 11:49:53 +0900

    [0326] 이정은

---
+++ welcome/welcome_prj/welcome_app/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import path
+from .views import *
+
+app_name = 'welcome_app'
+
+urlpatterns = [
+
+    path('', index, name = 'index'),
+    path('welcome/', welcome, name = 'welcome'),
+    
+]
